906a46214a44def2cb72d9075d292ff209c0dae1
diyinfosec <diyinfosec@gmail.com>
2020-06-01 17:23:31 +0100

    Update apfs_structures.h

--- Structures/apfs_structures.h
+++ Structures/apfs_structures.h
@@ -2,6 +2,7 @@ struct prange {
   paddr_t pr_start_paddr;
   uint64_t pr_block_count;
 };
+
 struct obj_phys {
   uint8_t o_cksum[MAX_CKSUM_SIZE];
   oid_t o_oid;
@@ -9,17 +10,20 @@ struct obj_phys {
   uint32_t o_type;
   uint32_t o_subtype;
 };
+
 struct checkpoint_map_phys {
   obj_phys_t cpm_o;
   uint32_t cpm_flags;
   uint32_t cpm_count;
   checkpoint_mapping_t cpm_map[];
 };
+
 struct evict_mapping_val {
   paddr_t dst_paddr;
   uint64_t len;
 }
 __attribute__((packed));
+
 struct omap_phys {
   obj_phys_t om_o;
   uint32_t om_flags;
@@ -30,22 +34,26 @@ struct omap_phys {
   oid_t om_snapshot_tree_oid;
   xid_t om_most_recent_snap;
   xid_t om_pending_revert_min;
-  omap_phys_t xid_t om_pending_revert_max;
+  xid_t om_pending_revert_max;
 };
+
 struct omap_key {
   oid_t ok_oid;
   xid_t ok_xid;
 };
+
 struct omap_val {
   uint32_t ov_flags;
   uint32_t ov_size;
   paddr_t ov_paddr;
 };
+
 struct omap_snapshot {
   uint32_t oms_flags;
   uint32_t oms_pad;
   oid_t oms_oid;
 };
+
 struct apfs_superblock {
   obj_phys_t apfs_o;
   uint32_t apfs_magic;
@@ -73,7 +81,7 @@ struct apfs_superblock {
   uint64_t apfs_num_symlinks;
   uint64_t apfs_num_other_fsobjects;
   uint64_t apfs_num_snapshots;
-  apfs_superblock_t uint64_t apfs_total_blocks_alloced;
+  uint64_t apfs_total_blocks_alloced;
   uint64_t apfs_total_blocks_freed;
   uuid_t apfs_vol_uuid;
   uint64_t apfs_last_mod_time;
@@ -91,19 +99,23 @@ struct apfs_superblock {
   oid_t apfs_snap_meta_ext_oid;
   uuid_t apfs_volume_group_id;
 };
+
 struct apfs_modified_by {
   uint8_t id[APFS_MODIFIED_NAMELEN];
   uint64_t timestamp;
   xid_t last_xid;
 };
+
 struct j_key {
   uint64_t obj_id_and_type;
 }
 __attribute__((packed));
+
 struct j_inode_key {
   j_key_t hdr;
 }
 __attribute__((packed));
+
 struct j_inode_val {
   uint64_t parent_id;
   uint64_t private_id;
@@ -127,29 +139,34 @@ struct j_inode_val {
   uint8_t xfields[];
 }
 __attribute__((packed));
+
 struct j_drec_key {
   j_key_t hdr;
   uint16_t name_len;
   uint8_t name[0];
 }
 __attribute__((packed));
+
 struct j_drec_hashed_key {
   j_key_t hdr;
   uint32_t name_len_and_hash;
   uint8_t name[0];
 }
 __attribute__((packed));
+
 struct j_drec_val {
   uint64_t file_id;
   uint64_t date_added;
   uint16_t flags;
-  j_dir_stats_key_t uint8_t xfields[];
+  uint8_t xfields[];
 }
 __attribute__((packed));
+
 struct j_dir_stats_key {
   j_key_t hdr;
 }
 __attribute__((packed));
+
 struct j_dir_stats_val {
   uint64_t num_children;
   uint64_t total_size;
@@ -157,71 +174,86 @@ struct j_dir_stats_val {
   uint64_t gen_count;
 }
 __attribute__((packed));
+
 struct j_xattr_key {
   j_key_t hdr;
   uint16_t name_len;
   uint8_t name[0];
 }
 __attribute__((packed));
+
 struct j_xattr_val {
   uint16_t flags;
   uint16_t xdata_len;
   uint8_t xdata[0];
 }
 __attribute__((packed));
+
 typedef enum {
   APFS_TYPE_ANY = 0, APFS_TYPE_SNAP_METADATA = 1, APFS_TYPE_EXTENT = 2, APFS_TYPE_INODE = 3, APFS_TYPE_XATTR = 4, APFS_TYPE_SIBLING_LINK = 5, APFS_TYPE_DSTREAM_ID = 6, APFS_TYPE_CRYPTO_STATE = 7, APFS_TYPE_FILE_EXTENT = 8, APFS_TYPE_DIR_REC = 9, APFS_TYPE_DIR_STATS = 10, APFS_TYPE_SNAP_NAME = 11, APFS_TYPE_SIBLING_MAP = 12, APFS_TYPE_MAX_VALID = 12, APFS_TYPE_MAX = 15, APFS_TYPE_INVALID = 15,
 }
 j_obj_types;
+
 typedef enum {
   APFS_KIND_ANY = 0, APFS_KIND_NEW = 1, APFS_KIND_UPDATE = 2, APFS_KIND_DEAD = 3, APFS_KIND_UPDATE_REFCNT = 4, APFS_KIND_INVALID = 255
 }
 j_obj_kinds;
+
 typedef enum {
   INODE_IS_APFS_PRIVATE = 0x00000001, INODE_MAINTAIN_DIR_STATS = 0x00000002, INODE_DIR_STATS_ORIGIN = 0x00000004, INODE_PROT_CLASS_EXPLICIT = 0x00000008, INODE_WAS_CLONED = 0x00000010, INODE_FLAG_UNUSED = 0x00000020, INODE_HAS_SECURITY_EA = 0x00000040, INODE_BEING_TRUNCATED = 0x00000080, INODE_HAS_FINDER_INFO = 0x00000100, INODE_IS_SPARSE = 0x00000200, INODE_WAS_EVER_CLONED = 0x00000400, INODE_ACTIVE_FILE_TRIMMED = 0x00000800, INODE_PINNED_TO_MAIN = 0x00001000, INODE_PINNED_TO_TIER2 = 0x00002000, INODE_HAS_RSRC_FORK = 0x00004000, INODE_NO_RSRC_FORK = 0x00008000, INODE_ALLOCATION_SPILLEDOVER = 0x00010000, INODE_FAST_PROMOTE = 0x00020000, INODE_HAS_UNCOMPRESSED_SIZE = 0x00040000, INODE_IS_PURGEABLE = 0x00080000, INODE_WANTS_TO_BE_PURGEABLE = 0x00100000, INODE_IS_SYNC_ROOT = 0x00200000, INODE_INHERITED_INTERNAL_FLAGS = (INODE_MAINTAIN_DIR_STATS), INODE_CLONED_INTERNAL_FLAGS = (INODE_HAS_RSRC_FORK\ | INODE_NO_RSRC_FORK\ | INODE_HAS_FINDER_INFO),
 }
 j_inode_flags;
+
 typedef enum {
-  XATTR_DATA_STREAM = 0x00000001, XATTR_DATA_EMBEDDED = 0x00000002, dir_rec_flags XATTR_FILE_SYSTEM_OWNED = 0x00000004, XATTR_RESERVED_8 = 0x00000008,
+  XATTR_DATA_STREAM = 0x00000001, XATTR_DATA_EMBEDDED = 0x00000002, XATTR_FILE_SYSTEM_OWNED = 0x00000004, XATTR_RESERVED_8 = 0x00000008,
 }
 j_xattr_flags;
+
 typedef enum {
   DREC_TYPE_MASK = 0x000f, RESERVED_10 = 0x0010
 }
 dir_rec_flags;
+
 struct j_phys_ext_key {
   j_key_t hdr;
 }
 __attribute__((packed));
+
 struct j_phys_ext_val {
   uint64_t len_and_kind;
   uint64_t owning_obj_id;
   int32_t refcnt;
 }
 __attribute__((packed));
+
 struct j_file_extent_key {
   j_key_t hdr;
   uint64_t logical_addr;
 }
 __attribute__((packed));
+
 struct j_file_extent_val {
   uint64_t len_and_flags;
   uint64_t phys_block_num;
   uint64_t crypto_id;
 }
 __attribute__((packed));
+
 struct j_dstream_id_key {
   j_key_t hdr;
 }
 __attribute__((packed));
+
 struct j_dstream_id_val {
   uint32_t refcnt;
 }
 __attribute__((packed));
+
 struct j_xattr_dstream {
   uint64_t xattr_obj_id;
   j_dstream_t dstream;
 };
+
 struct j_dstream {
   uint64_t size;
   uint64_t alloced_size;
@@ -230,34 +262,41 @@ struct j_dstream {
   uint64_t total_bytes_read;
 }
 __attribute__((aligned(8), packed));
+
 struct x_field {
   uint8_t x_type;
   uint8_t x_flags;
   uint16_t x_size;
 };
+
 struct j_sibling_key {
   j_key_t hdr;
   uint64_t sibling_id;
 }
 __attribute__((packed));
+
 struct j_sibling_val {
   uint64_t parent_id;
   uint16_t name_len;
   uint8_t name[0];
 }
 __attribute__((packed));
+
 struct j_sibling_map_key {
   j_key_t hdr;
 }
 __attribute__((packed));
+
 struct j_sibling_map_val {
   uint64_t file_id;
 }
 __attribute__((packed));
+
 struct j_snap_metadata_key {
   j_key_t hdr;
 }
 __attribute__((packed));
+
 struct j_snap_metadata_val {
   oid_t extentref_tree_oid;
   oid_t sblock_oid;
@@ -270,24 +309,29 @@ struct j_snap_metadata_val {
   uint8_t name[0];
 }
 __attribute__((packed));
+
 struct j_snap_name_key {
   j_key_t hdr;
   uint16_t name_len;
   uint8_t name[0];
 }
 __attribute__((packed));
+
 struct j_snap_name_val {
   xid_t snap_xid;
 }
 __attribute__((packed));
+
 typedef enum {
-  SNAP_META_PENDING_DATALESS = 0x00000001, SNAP_META_MERGE_IN_PROGRESS = 0x00000002, snap_meta_ext_obj_phys_t
+  SNAP_META_PENDING_DATALESS = 0x00000001, SNAP_META_MERGE_IN_PROGRESS = 0x00000002,
 }
 snap_meta_flags;
+
 struct snap_meta_ext_obj_phys {
   obj_phys_t smeop_o;
   snap_meta_ext_t smeop_sme;
 }
+
 typedef struct snap_meta_ext {
   uint32_t sme_version;
   uint32_t sme_flags;
@@ -296,6 +340,7 @@ typedef struct snap_meta_ext {
   uint64_t sme_token;
 }
 __attribute__((packed))
+
 struct btree_node_phys {
   obj_phys_t btn_o;
   uint16_t btn_flags;
@@ -306,14 +351,15 @@ struct btree_node_phys {
   nloc_t btn_key_free_list;
   nloc_t btn_val_free_list;
   uint64_t btn_data[];
-  btree_node_phys_t
 };
+
 struct btree_info_fixed {
   uint32_t bt_flags;
   uint32_t bt_node_size;
   uint32_t bt_key_size;
   uint32_t bt_val_size;
 };
+
 struct btree_info {
   btree_info_fixed_t bt_fixed;
   uint32_t bt_longest_key;
@@ -321,27 +367,33 @@ struct btree_info {
   uint64_t bt_key_count;
   uint64_t bt_node_count;
 };
+
 struct nloc {
   uint16_t off;
   uint16_t len;
 };
+
 struct kvloc {
   nloc_t k;
   nloc_t v;
 };
+
 struct kvoff {
   uint16_t k;
   uint16_t v;
 };
+
 struct j_crypto_key {
   j_key_t hdr;
 }
 __attribute__((packed));
+
 struct j_crypto_val {
   uint32_t refcnt;
   wrapped_crypto_state_t state;
 }
 __attribute__((aligned(4), packed));
+
 struct wrapped_crypto_state {
   uint16_t major_version;
   uint16_t minor_version;
@@ -353,6 +405,7 @@ struct wrapped_crypto_state {
   uint8_t persistent_key[0];
 }
 __attribute__((aligned(2), packed));
+
 struct wrapped_meta_crypto_state {
   uint16_t major_version;
   uint16_t minor_version;
@@ -363,6 +416,7 @@ struct wrapped_meta_crypto_state {
   uint16_t unused;
 }
 __attribute__((aligned(2), packed));
+
 struct kb_locker {
   uint16_t kl_version;
   uint16_t kl_nkeys;
@@ -370,6 +424,7 @@ struct kb_locker {
   uint8_t padding[8];
   keybag_entry_t kl_entries[];
 };
+
 struct keybag_entry {
   uuid_t ke_uuid;
   uint16_t ke_tag;
@@ -377,10 +432,12 @@ struct keybag_entry {
   uint8_t padding[4];
   uint8_t ke_keydata[];
 };
+
 struct media_keybag {
   obj_phys_t mk_obj;
   kb_locker_t mk_locker;
 };
+
 struct chunk_info {
   uint64_t ci_xid;
   uint64_t ci_addr;
@@ -388,22 +445,26 @@ struct chunk_info {
   uint32_t ci_free_count;
   paddr_t ci_bitmap_addr;
 };
+
 struct chunk_info_block {
   obj_phys_t cib_o;
   uint32_t cib_index;
   uint32_t cib_chunk_info_count;
   chunk_info_t cib_chunk_info[];
 };
+
 struct cib_addr_block {
   obj_phys_t cab_o;
   uint32_t cab_index;
   uint32_t cab_cib_count;
   paddr_t cab_cib_addr[];
 };
+
 struct spaceman_free_queue_key {
   xid_t sfqk_xid;
   paddr_t sfqk_paddr;
 };
+
 struct spaceman_free_queue {
   uint64_t sfq_count;
   oid_t sfq_tree_oid;
@@ -413,6 +474,7 @@ struct spaceman_free_queue {
   uint32_t sfq_pad32;
   uint64_t sfq_reserved;
 };
+
 struct spaceman_device {
   uint64_t sm_block_count;
   uint64_t sm_chunk_count;
@@ -423,10 +485,12 @@ struct spaceman_device {
   uint32_t sm_reserved;
   uint64_t sm_reserved2;
 };
+
 struct spaceman_allocation_zone_boundaries {
   uint64_t saz_zone_start;
   uint64_t saz_zone_end;
 };
+
 struct spaceman_allocation_zone_info_phys {
   spaceman_allocation_zone_boundaries_t saz_current_boundaries;
   spaceman_allocation_zone_boundaries_t saz_previous_boundaries[SM_ALLOCZONE_NUM_PREVIOUS_BOUNDARIES];
@@ -434,9 +498,11 @@ struct spaceman_allocation_zone_info_phys {
   uint16_t saz_previous_boundary_index;
   uint32_t saz_reserved;
 };
+
 struct spaceman_datazone_info_phys {
   spaceman_allocation_zone_info_phys_t sdz_allocation_zones[SD_COUNT][SM_DATAZONE_ALLOCZONE_COUNT];
 };
+
 struct spaceman_phys {
   obj_phys_t sm_o;
   uint32_t sm_block_size;
@@ -452,7 +518,7 @@ struct spaceman_phys {
   paddr_t sm_ip_bm_base;
   paddr_t sm_ip_base;
   uint64_t sm_fs_reserve_block_count;
-  sfq uint64_t sm_fs_reserve_alloc_count;
+  uint64_t sm_fs_reserve_alloc_count;
   spaceman_free_queue_t sm_fq[SFQ_COUNT];
   uint16_t sm_ip_bm_free_head;
   uint16_t sm_ip_bm_free_tail;
@@ -463,6 +529,7 @@ struct spaceman_phys {
   uint32_t sm_struct_size;
   spaceman_datazone_info_phys_t sm_datazone;
 };
+
 struct nx_reaper_phys {
   obj_phys_t nr_o;
   uint64_t nr_next_reap_id;
@@ -480,6 +547,7 @@ struct nx_reaper_phys {
   uint32_t uint8_t nr_state_buffer_size;
   nr_state_buffer[];
 };
+
 struct nx_reap_list_phys {
   obj_phys_t nrl_o;
   oid_t nrl_next;
@@ -491,6 +559,7 @@ struct nx_reap_list_phys {
   uint32_t nrl_free;
   nx_reap_list_entry_t nrl_entries[];
 };
+
 struct nx_reap_list_entry {
   uint32_t nrle_next;
   uint32_t nrle_flags;
@@ -500,10 +569,12 @@ struct nx_reap_list_entry {
   oid_t nrle_oid;
   xid_t nrle_xid;
 };
+
 struct omap_reap_state {
   uint32_t omr_phase;
   omap_key_t omr_ok;
 };
+
 struct omap_cleanup_state {
   uint32_t omc_cleaning;
   uint32_t omc_omsflags;
@@ -513,13 +584,14 @@ struct omap_cleanup_state {
   xid_t omc_sxidnext;
   omap_key_t omc_curkey;
 };
+
 struct apfs_reap_state {
   uint64_t last_pbn;
   xid_t cur_snap_xid;
   uint32_t phase;
-  apfs_reap_state_t
 }
 __attribute__((packed));
+
 struct er_state_phys {
   er_state_phys_header_t ersb_header;
   uint64_t ersb_flags;
@@ -534,6 +606,7 @@ struct er_state_phys {
   oid_t ersb_recovery_list_oid;
   uint64_t ersb_recovery_length;
 };
+
 struct er_state_phys_v1 {
   er_state_phys_header_t ersb_header;
   uint64_t ersb_flags;
@@ -550,27 +623,28 @@ struct er_state_phys_v1 {
   uint64_t ersb_fext_cid;
   uint8_t ersb_checksum[0];
 };
-struct er_state_phys_header {
-  obj_phys_t ersb_o;
-  uint32_t ersb_magic;
-  uint32_t ersb_version;
-};
+
+struct er_state_phys_header {};
+
 struct er_recovery_block_phys {
   obj_phys_t erb_o;
   uint64_t erb_offset;
   oid_t erb_next_oid;
   uint8_t erb_data[0];
 };
+
 struct gbitmap_block_phys {
   obj_phys_t bmb_o;
   uint64_t bmb_field[0];
 };
+
 struct gbitmap_phys {
   obj_phys_t bm_o;
   oid_t bm_tree_oid;
   uint64_t bm_bit_count;
   uint64_t bm_flags;
 };
+
 typedef struct {
   obj_phys_t fwp_objHdr;
   uint64_t fwp_version;
@@ -584,12 +658,14 @@ typedef struct {
   prange_t fwp_rcStash;
 }
 fusion_wbc_phys_t;
+
 typedef struct {
   paddr_t fwle_wbcLba;
   paddr_t fwle_targetLba;
   uint64_t fwle_length;
 }
 fusion_wbc_list_entry_t;
+
 typedef struct {
   obj_phys_t fwlp_objHdr;
   uint64_t fwlp_version;
@@ -601,6 +677,7 @@ typedef struct {
   fusion_wbc_list_entry_t fwlp_listEntries[];
 }
 fusion_wbc_list_phys_t;
+
 typedef struct {
   paddr_t fmv_lba;
   uint32_t fmv_length;
